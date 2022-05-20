c84c8bf806a81ffe04e8ec86a94d83d648f02a1e
Leslie <103486449+LeslieFritas95@users.noreply.github.com>
2022-05-20 13:29:39 +0200

    date_deleteButton

--- script.js
+++ script.js
@@ -1,25 +1,136 @@
+const responseCallBack = (response) => response.json();
 
-fetch('https://62860d1af0e8f0bb7c0f4273.mockapi.io/students')
-.then(responseCallBack)
-.then(resultCallback)
-.catch(manageError);
-
-
-function responseCallBack(response){
-  console.log('response',response);
-  return response.json();
+const createAvatarImgFromSrc = (src) => {
+  const image = document.createElement('img');
+  image.classList.add('avatar-image');
+  image.src = src;
+  return image;
 }
 
-function manageError(error) {
-  console.log(error)
+
+const createTextSpan = (text) => {
+  const span = document.createElement('span');
+  span.appendChild(document.createTextNode(text));
+  return span;
 }
 
-function resultCallback(result){
-  console.log('result', result);
-  const array = convertResultInArrayOfStudents(result);
-  console.log('array', array);
-  displayStudents(result);
+const deleteCallback = () => {
+  initApp();
 }
+
+const deleteStudent = (id) => {
+  const deleteUrl = 'https://62860d1af0e8f0bb7c0f4273.mockapi.io/students/' + id;
+  const fetchConf = {
+    method: 'delete'
+  }
+  fetch(deleteUrl, fetchConf)
+  .then(responseCallBack)
+  .then(deleteCallback);
+}
+
+const createDeleteButton = (id) => {
+  const button = document.createElement('button');
+  button.onclick = () => deleteStudent(id)
+  const node = document.createTextNode('cancella');
+  button.appendChild(node);
+  return button;
+}
+
+const createStudentCard = (student) => {
+  const studentCard = document.createElement('div');
+  studentCard.classList.add('student-card');
+  studentCard.appendChild(createAvatarImgFromSrc(student.avatar));
+  studentCard.appendChild(createTextSpan(student.name + ' ' + student.surname));
+  studentCard.appendChild(createTextSpan('Giorni al compleanno: ' + student.getDaysToBirthday()))
+  studentCard.appendChild(createDeleteButton(student.id));
+  return studentCard;
+}
+
+const createArrayOfStudentCard = (arrayOfStudents) => arrayOfStudents.map(student => createStudentCard(student));
+
+
+const displayStudents = (arrayOfStudents) => {
+  document.body.innerHTML = '';
+  const arrayContainer = document.createElement('div');
+  arrayContainer.append(...createArrayOfStudentCard(arrayOfStudents));
+  document.body.appendChild(arrayContainer);
+}
+
+const convertResultInArrayOfStudents = (result) => result.map(obj => Student.fromObj(obj));
+
+const resultCallback = (result) => displayStudents(convertResultInArrayOfStudents(result));
+
+const catchError = (error) => console.log(error);
+
+const initiApp = () => fetch('https://62860d1af0e8f0bb7c0f4273.mockapi.io/students')
+                       .then(responseCallBack)
+                       .then(resultCallback)
+                       .catch(catchError);
+
+initiApp();
+// function createTextSpan(text){
+//   const span = document.createElement('span');
+//   span.appendChild(document.createTextNode(text));
+//   return span;
+
+// }
+
+
+// function displayStudents(arrayOfStudents){
+//   const arrayContainer = document.createElement('div');
+
+//   for (let i = 0; i < arrayOfStudents.length; i++) {
+//     const student = arrayOfStudents[i]
+//     const studentContainer = document.createElement('div');
+//     studentContainer.appendChild(createTextSpan(student.name + ' ' + student.surname));
+//     // const span = document.createTextSpan(student.name + ' ' + student.surname);
+
+   
+//     // studentContainer.appendChild(span);
+//     arrayContainer.appendChild(studentContainer);
+
+//   }
+
+//   document.body.appendChild(arrayContainer);
+
+// }
+
+// function displayStudents(arrayOfStudents){
+//   const arrayContainer = document.createElement('div');
+
+//   for (let i = 0; i < arrayOfStudents.length; i++) {
+//     const student = arrayOfStudents[i]
+//     const studentContainer = document.createElement('div');
+//     const span = document.createElement('span');
+//     const node = document.createTextNode(student.name + ' ' + student.surname);
+
+//     span.appendChild(node);
+//     studentContainer.appendChild(span);
+//     arrayContainer.appendChild(studentContainer);
+
+//   }
+
+//   document.body.appendChild(arrayContainer);
+
+// }
+
+
+
+// function responseCallBack(response){
+//   console.log('response',response);
+//   return response.json();
+// }
+
+// function manageError(error) {
+//   console.log(error)
+// }
+
+// function resultCallback(result){
+//   // console.log('result', result);
+//   const array = convertResultInArrayOfStudents(result);
+//   // console.log('array', array);
+//   displayStudents(array);
+// }
 
 // function convertResultInArrayOfStudents(result) {
 //   const arrayOfStudents = [];
@@ -39,29 +150,10 @@ function resultCallback(result){
 //   return arrayOfStudents;
 // }
 
-function convertResultInArrayOfStudents(result) {
-  const arrayOfStudents = result.map(obj => student.fromObj(obj));
-  return arrayOfStudents;
-}
-
-function displayStudents(arrayOfStudents){
-  const arrayContainer = document.createElement('div');
-
-  for (let i = 0; i < arrayOfStudents.length; i++) {
-    const student = arrayOfStudents[i]
-    const studentContainer = document.createElement('div');
-    const span = document.createElement('span');
-    const node = document.createTextNode(student.name + ' ' + student.surname);
-
-    span.appendChild(node);
-    studentContainer.appendChild(span);
-    arrayContainer.appendChild(studentContainer);
-
-  }
-
-  document.body.appendChild(arrayContainer);
-
-}
+// function convertResultInArrayOfStudents(result) {
+//   const arrayOfStudents = result.map(obj => Student.fromObj(obj));
+//   return arrayOfStudents;
+// }
 
 
 
--- student.js
+++ student.js
@@ -1,11 +1,11 @@
 class Student{
 
-    constructor(id, name, surname, dob, avatarUrl){
+    constructor(id, name, surname, dob, avatar){
       this.id = id;
       this.name = name;
       this.surname = surname;
       this.dob = dob;
-      this.avatarUrl = avatarUrl;
+      this.avatar = avatar;
     }
   
     get dateOfBirth(){
@@ -21,7 +21,24 @@ class Student{
     }
   
     getDaysToBirthday(){
-      
+    const today = new Date()
+    const todayMills = today.getTime();
+    const birthdayClone = new Date(this.dateOfBirth.getTime());
+    birthdayClone.setFullYear(new Date().getFullYear());
+    const birthdayInMills = birthdayClone.getTime();
+
+    let diff;
+    if (todayMills > birthdayInMills) {
+      birthdayClone.setFullYear(new Date().getFullYear() + 1);
+      const newYearBirthdayInMills = birthdayClone.getTime();
+      diff = newYearBirthdayInMills - todayMills;
+    } else {
+      diff = birthdayInMills - todayMills;
+    }
+
+    const daysToBirth = diff / 1000 / 60 / 60 / 24;
+    const flooredDateToBirth = Math.floor(daysToBirth);
+    return flooredDateToBirth;
     }
   
   }
